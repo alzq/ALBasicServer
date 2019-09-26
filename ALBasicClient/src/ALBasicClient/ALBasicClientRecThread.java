7592e859d0faa2792d36f4d6cedfec5c14c54d80
alzq.z <alzq.z@mechanist.co>
2019-09-26 10:52:01 +0800

    add catch for read thread

--- ALBasicClient/src/ALBasicClient/ALBasicClientRecThread.java
+++ ALBasicClient/src/ALBasicClient/ALBasicClientRecThread.java
@@ -108,53 +108,63 @@ public class ALBasicClientRecThread extends _AALBasicThread
                 e.printStackTrace();
             }
             
-            //循环获取到的事件
-            Set<SelectionKey> readyKeySet = clientSelector.selectedKeys();
-            Iterator<SelectionKey> iter = readyKeySet.iterator();
-            while(iter.hasNext())
+            try
             {
-                SelectionKey key = (SelectionKey)iter.next();
-                //移除已经取出的事件
-                iter.remove();
-                
-                if(key.isReadable())
-                {
-                    //收到数据
-                    SocketChannel socketChannel = (SocketChannel)key.channel();
-
-                    //读取数据
-                    try
-                    {
-                        recBuffer.clear();
-                        //此时进行读取，当读取的包大小超过存储区域则可能导致数据读取不全
-                        int recLen = socketChannel.read(recBuffer);
-                        //设置限制，并将读取位置归0
-                        recBuffer.flip();
-                        
-                        if(recLen < 0)
-                        {
-                            _m_scSocket._logout();
-                            return ;
-                        }
-                        
-                        if(recLen > 0)
-                        {
-                            //处理消息读取
-                            _m_scSocket._socketReceivingMessage(recBuffer);
-                        }
-                    }
-                    catch (IOException e)
-                    {
-                        _m_scSocket._logout();
-                        return ;
-                    }
-                    catch (Exception e)
-                    {
-                        ALServerLog.Fatal("Client port read buffer error!!");
-                        e.printStackTrace();
-                    }
-                }
+	            //循环获取到的事件
+	            Set<SelectionKey> readyKeySet = clientSelector.selectedKeys();
+	            Iterator<SelectionKey> iter = readyKeySet.iterator();
+	            while(iter.hasNext())
+	            {
+	                SelectionKey key = (SelectionKey)iter.next();
+	                //移除已经取出的事件
+	                iter.remove();
+	                
+	                if(key.isReadable())
+	                {
+	                    //收到数据
+	                    SocketChannel socketChannel = (SocketChannel)key.channel();
+	
+	                    //读取数据
+	                    try
+	                    {
+	                        recBuffer.clear();
+	                        //此时进行读取，当读取的包大小超过存储区域则可能导致数据读取不全
+	                        int recLen = socketChannel.read(recBuffer);
+	                        //设置限制，并将读取位置归0
+	                        recBuffer.flip();
+	                        
+	                        if(recLen < 0)
+	                        {
+	                            _m_scSocket._logout();
+	                            return ;
+	                        }
+	                        
+	                        if(recLen > 0)
+	                        {
+	                            //处理消息读取
+	                            _m_scSocket._socketReceivingMessage(recBuffer);
+	                        }
+	                    }
+	                    catch (IOException e)
+	                    {
+	                        _m_scSocket._logout();
+	                        return ;
+	                    }
+	                    catch (Exception e)
+	                    {
+	                        ALServerLog.Fatal("Client port read buffer error!!");
+	                        e.printStackTrace();
+	                    }
+	                }
+	            }
             }
+            catch (Exception e) {
+            	if(!_m_bThreadExit)
+            	{
+	            	ALServerLog.Fatal("Rec Thread Read Error!");
+					e.printStackTrace();
+            	}
+			}
         }
     }
 }
